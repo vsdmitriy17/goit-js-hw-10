eb7076a41bad4c940207dc632bc7df6d2388073b
Dmitriy Voskoboynikov <voskoboynikov010684@gmail.com>
2022-02-10 20:39:35 +0200

    descr/fix

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -6,9 +6,9 @@ const searchParams = new URLSearchParams({
 });
 
 // Ф-ция для составления запросов к серверу API, принимает параметр name (строка - ресурс, к которому мы обращаемся в API), возвращает результат вызова метода fetch() - промис, который
-//    вызывает метод then() и передает результат промиса (fulfilled) данные - в параметр (response) метода, который:
-//             - если response не оk, принудительно вызывает ошибку throw new Error(response.status) - error, т.е. формирует результат rejected;
-//             - иначе возвращает полученные данные в JSON формате response.json()
+//    передает результат промиса (fulfilled) данные - в параметр (response) метода then(), который:
+//             - если response не оk, принудительно вызывает ошибку (throw new Error(response.status)) - error, т.е. формирует результат rejected;
+//             - иначе возвращает полученные данные (из JSON формата) response.json()
 function fetchCountries(name) {
     return fetch(`${BASE_URL}/name/${name}?${searchParams}`)
     .then(response => {
